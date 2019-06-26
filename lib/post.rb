89c101f0cfda682221cceda1a7d81be5a9ead7c7
Eric Clark <eclark0013@gmail.com>
2019-06-26 18:03:42 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -12,6 +12,14 @@ class Post
     @@all
   end
 
+
+  def author=
+    # assign post to the author instance of the Author class
+    # so that when you call that author instance, he knows that the post to which he has been assigned
+    # is one that he wrote
+    @author.add_post.self
+  end
+    
   def author_name
     self.author
   end
